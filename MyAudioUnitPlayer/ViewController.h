5b7f3b7e11fde69c4b0088be46bc01d3f784d8a7
yanminli <lym_ay@sina.com>
2018-07-06 14:26:02 +0800

    Initial commit

---
+++ MyAudioUnitPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAudioUnitPlayer
+//
+//  Created by olami on 2018/7/6.
+//  Copyright © 2018年 VIA Technologies, Inc. & OLAMI Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyAudioUnitPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyAudioUnitPlayer
+//
+//  Created by olami on 2018/7/6.
+//  Copyright © 2018年 VIA Technologies, Inc. & OLAMI Team. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
